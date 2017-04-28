75e9632c3859fd098f036efd193eced343a9f385
poldrack <poldrack@gmail.com>
2017-04-28 12:29:40 -0700

    added clustering code

--- neurosynth/2.clustering.py
+++ neurosynth/2.clustering.py
@@ -1,7 +1,9 @@
 # cluster data for dimensionality reduction
 # for use in encoding model
+# based on: https://nilearn.github.io/auto_examples/03_connectivity/plot_rest_clustering.html#sphx-glr-auto-examples-03-connectivity-plot-rest-clustering-py
 
 from nilearn import input_data
+import pickle
 
 # The NiftiMasker will extract the data on a mask. We do not have a
 # mask, hence we need to compute one.
@@ -20,3 +22,30 @@ fmri_masked = nifti_masker.fit_transform(func_filename)
 
 # We can retrieve the numpy array of the mask
 mask = nifti_masker.mask_img_.get_data().astype(bool)
+
+# Compute connectivity matrix: which voxel is connected to which
+from sklearn.feature_extraction import image
+shape = mask.shape
+connectivity = image.grid_to_graph(n_x=shape[0], n_y=shape[1],
+                                   n_z=shape[2], mask=mask)
+
+# Computing the ward for the first time, this is long...
+from sklearn.cluster import FeatureAgglomeration
+# If you have scikit-learn older than 0.14, you need to import
+# WardAgglomeration instead of FeatureAgglomeration
+import time
+start = time.time()
+ward = FeatureAgglomeration(n_clusters=1000, connectivity=connectivity,
+                            linkage='ward', memory='nilearn_cache')
+ward.fit(fmri_masked)
+print("Ward agglomeration 1000 clusters: %.2fs" % (time.time() - start))
+
+# Compute the ward with more clusters, should be faster as we are using
+# the caching mechanism
+start = time.time()
+ward = FeatureAgglomeration(n_clusters=2000, connectivity=connectivity,
+                            linkage='ward', memory='nilearn_cache')
+ward.fit(fmri_masked)
+print("Ward agglomeration 2000 clusters: %.2fs" % (time.time() - start))
+
+pickle.dump(ward,open('ward.pkl','wb'))
