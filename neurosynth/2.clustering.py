eea72455878b6b64571449dfafddb2b93fb22c39
poldrack <poldrack@gmail.com>
2017-04-28 14:50:31 -0700

    save reduced data

--- neurosynth/2.clustering.py
+++ neurosynth/2.clustering.py
@@ -4,48 +4,98 @@
 
 from nilearn import input_data
 import pickle
+import os
+from sklearn.cluster import FeatureAgglomeration
 
-# The NiftiMasker will extract the data on a mask. We do not have a
-# mask, hence we need to compute one.
-#
-# This is resting-state data: the background has not been removed yet,
-# thus we need to use mask_strategy='epi' to compute the mask from the
-# EPI images
 nifti_masker = input_data.NiftiMasker(memory='nilearn_cache',
                                       mask_strategy='epi', memory_level=1,
                                       standardize=False)
+n_clusters=2000
 
-func_filename = 'all_ns_data.nii.gz'
-# The fit_transform call computes the mask and extracts the time-series
-# from the files:
-fmri_masked = nifti_masker.fit_transform(func_filename)
+if os.path.exists('ward.pkl'):
+    print('loading saved ward clustering')
+    ward=pickle.load(open('ward.pkl','rb'))
+else:
+    print('runnign ward clustering')
+    # The NiftiMasker will extract the data on a mask. We do not have a
+    # mask, hence we need to compute one.
+    #
+    # This is resting-state data: the background has not been removed yet,
+    # thus we need to use mask_strategy='epi' to compute the mask from the
+    # EPI images
 
-# We can retrieve the numpy array of the mask
-mask = nifti_masker.mask_img_.get_data().astype(bool)
+    func_filename = 'all_ns_data.nii.gz'
+    # The fit_transform call computes the mask and extracts the time-series
+    # from the files:
+    fmri_masked = nifti_masker.fit_transform(func_filename)
 
-# Compute connectivity matrix: which voxel is connected to which
-from sklearn.feature_extraction import image
-shape = mask.shape
-connectivity = image.grid_to_graph(n_x=shape[0], n_y=shape[1],
-                                   n_z=shape[2], mask=mask)
+    # We can retrieve the numpy array of the mask
+    mask = nifti_masker.mask_img_.get_data().astype(bool)
 
-# Computing the ward for the first time, this is long...
-from sklearn.cluster import FeatureAgglomeration
-# If you have scikit-learn older than 0.14, you need to import
-# WardAgglomeration instead of FeatureAgglomeration
-import time
-start = time.time()
-ward = FeatureAgglomeration(n_clusters=1000, connectivity=connectivity,
-                            linkage='ward', memory='nilearn_cache')
-ward.fit(fmri_masked)
-print("Ward agglomeration 1000 clusters: %.2fs" % (time.time() - start))
+    # Compute connectivity matrix: which voxel is connected to which
+    from sklearn.feature_extraction import image
+    shape = mask.shape
+    connectivity = image.grid_to_graph(n_x=shape[0], n_y=shape[1],
+                                       n_z=shape[2], mask=mask)
 
-# Compute the ward with more clusters, should be faster as we are using
-# the caching mechanism
-start = time.time()
-ward = FeatureAgglomeration(n_clusters=2000, connectivity=connectivity,
-                            linkage='ward', memory='nilearn_cache')
-ward.fit(fmri_masked)
-print("Ward agglomeration 2000 clusters: %.2fs" % (time.time() - start))
+    # Computing the ward for the first time, this is long...
+    # If you have scikit-learn older than 0.14, you need to import
+    # WardAgglomeration instead of FeatureAgglomeration
+    import time
+    start = time.time()
+    ward = FeatureAgglomeration(n_clusters=n_clusters, connectivity=connectivity,
+                                linkage='ward', memory='nilearn_cache')
+    ward.fit(fmri_masked)
+    print("Ward agglomeration 2000 clusters: %.2fs" % (time.time() - start))
 
-pickle.dump(ward,open('ward.pkl','wb'))
+    pickle.dump(ward,open('ward.pkl','wb'))
+
+    fmri_reduced = ward.transform(fmri_mean)
+
+    pickle.dump(fmri_reduced,open('neurosynth_reduced.pkl','wb'))
+
+
+
+# plot results
+
+from nilearn.plotting import plot_roi, plot_epi, show
+fmri_mean = nifti_masker.fit_transform('mean_ns_data.nii.gz')
+
+# Unmask the labels
+
+# Avoid 0 label
+labels = ward.labels_ + 1
+labels_img = nifti_masker.inverse_transform(labels)
+
+from nilearn.image import mean_img
+mean_func_img = 'mean_ns_data.nii.gz'
+
+
+first_plot = plot_roi(labels_img, mean_func_img, title="Ward parcellation",
+                      display_mode='xz')
+
+# common cut coordinates for all plots
+cut_coords = first_plot.cut_coords
+labels_img.to_filename('parcellation.nii.gz')
+
+# Display the original data
+plot_epi(nifti_masker.inverse_transform(fmri_mean),
+         cut_coords=cut_coords,
+         title='Original (%i voxels)' % fmri_mean.shape[1],
+         vmax=fmri_mean.max(), vmin=fmri_mean.min(),
+         display_mode='xz')
+
+# A reduced data can be create by taking the parcel-level average:
+# Note that, as many objects in the scikit-learn, the ward object exposes
+# a transform method that modifies input features. Here it reduces their
+# dimension
+fmri_reduced = ward.transform(fmri_mean)
+
+# Display the corresponding data compressed using the parcellation
+fmri_compressed = ward.inverse_transform(fmri_reduced)
+compressed_img = nifti_masker.inverse_transform(fmri_compressed)
+
+plot_epi(compressed_img, cut_coords=cut_coords,
+         title='Compressed representation (2000 parcels)',
+         vmax=fmri_mean.max(), vmin=fmri_mean.min(),
+         display_mode='xz')
