8d9ad1de7b5fe6ddd83e17f85e2bd542de68e6fd
Russell Poldrack <poldrack@gmail.com>
2017-05-01 18:54:35 -0500

    added dims

--- pubmed_word2vec/get_dim.py
+++ pubmed_word2vec/get_dim.py
@@ -1,5 +1,5 @@
 import numpy
-for d in [25,50,75,100,125,150,175,200]:
+for d in [5,10,15,20,25,30,35,40,45,50,75,100,125,150,175,200]:
   try:
     s=numpy.load('scores_%d_dims.npy'%d)
     print(d,numpy.mean(s))
