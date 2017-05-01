09e148d0de289375f5c6e731728c3c35489f96d7
poldrack <poldrack@gmail.com>
2017-05-01 12:06:54 -0700

    do vocab for every run

--- pubmed_word2vec/2.1.estimate_doc2vec_dimensionality.py
+++ pubmed_word2vec/2.1.estimate_doc2vec_dimensionality.py
@@ -19,18 +19,6 @@ doc_td=pickle.load(open('doc_td.pkl','rb'))
 docs=list(range(len(doc_td)))
 
 kf = KFold(n_splits=2)
-ndims=200
-
-if os.path.exists('doc2vec_trigram_vocab.model'):
-    print("using saved vocabulary")
-    model_docs=Doc2Vec.load('doc2vec_trigram_vocab.model')
-else:
-    print('learning vocabulary')
-    model_docs=Doc2Vec(dm=1, size=ndims, window=5, negative=0,
-            hs=1, min_count=50, workers=22,iter=20,
-            alpha=0.025, min_alpha=0.025,dbow_words=1)
-    model_docs.build_vocab(doc_td)
-    model_docs.save('doc2vec_trigram_vocab.model')
 
 try:
     ndims=int(sys.argv[1])
@@ -38,7 +26,12 @@ except:
     ndims=100
     print('using default ndims=',ndims)
 
-model_docs.vector_size=ndims
+print('learning vocabulary')
+model_docs=Doc2Vec(dm=1, size=ndims, window=5, negative=0,
+        hs=1, min_count=50, workers=22,iter=20,
+        alpha=0.025, min_alpha=0.025,dbow_words=1)
+model_docs.build_vocab(doc_td)
+
 scores=numpy.zeros(len(doc_td))
 
 for train, test in kf.split(docs):
@@ -47,7 +40,7 @@ for train, test in kf.split(docs):
     print('learning model')
     for epoch in range(10):
         random.shuffle(train_docs)
-        print('training on',model_docs.alpha)
+        print(ndims,'training on',model_docs.alpha)
         model_docs.train(train_docs,total_examples=len(train_docs),
                             epochs=model_docs.iter)
         model_docs.alpha-=.002
