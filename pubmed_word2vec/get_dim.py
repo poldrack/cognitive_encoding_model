995838c0af41551ebe52cf2f8410765055213177
poldrack <poldrack@gmail.com>
2017-05-02 06:46:29 -0700

    added plot

--- pubmed_word2vec/get_dim.py
+++ pubmed_word2vec/get_dim.py
@@ -1,7 +1,17 @@
 import numpy
+import matplotlib.pyplot as plt
+
+scores={}
 for d in [5,10,15,20,25,30,35,40,45,50,75,100,125,150,175,200]:
   try:
     s=numpy.load('scores_%d_dims.npy'%d)
-    print(d,numpy.mean(s))
+    scores[d]=numpy.mean(s)
+    print(d,scores[d])
   except:
     print('no data for',d)
+
+dvals=list(scores.keys())
+dvals.sort()
+
+plt.plot(dvals,[scores[x] for x in dvals])
+plt.show()
