7e3679ee0a247908eddcce3beb678b4f65235e93
poldrack <poldrack@gmail.com>
2017-05-11 16:18:37 -0700

    change directory for output, remove deduplication

--- neurosynth/3.project_neurosynth.py
+++ neurosynth/3.project_neurosynth.py
@@ -13,15 +13,14 @@ import nltk
 from Bio import Entrez
 
 Entrez.email='poldrack@stanford.edu'
-deduplicate=False
 force_new=False
 
-if os.path.exists('../neurosynth/data/abstract_text.pkl') and not force_new:
+if os.path.exists('data/abstract_text.pkl') and not force_new:
     print('loading existing abstracts')
-    abstracts,bad_pmids=pickle.load(open('../neurosynth/data/abstract_text.pkl','rb'))
+    abstracts,bad_pmids=pickle.load(open('data/abstract_text.pkl','rb'))
 else:
     print('getting abstracts')
-    desmtx=pandas.read_csv('../neurosynth/data/desmtx.csv',index_col=0)
+    desmtx=pandas.read_csv('data/desmtx.csv',index_col=0)
     pmids=list(desmtx.index)
     abstracts={}
     bad_pmids=[]
@@ -39,22 +38,17 @@ else:
 
     print(': found %d abstracts from %d keys'%(len(abstracts),len(pmids)))
     print('saving abstracts')
-    pickle.dump((abstracts,bad_pmids),open('../neurosynth/data/abstract_text.pkl','wb'))
+    pickle.dump((abstracts,bad_pmids),open('data/abstract_text.pkl','wb'))
 
 
 try:
     assert not force_new
-    if deduplicate:
-        desmtx=pandas.read_csv('../neurosynth/data/desmtx_cleaned_deduped.csv',index_col=0)
-        data=pickle.load(open('../neurosynth/neurosynth_reduced_cleaned_deduped.pkl','rb'))
-        print('loading cleaned/deduplicated data')
-    else:
-        desmtx=pandas.read_csv('../neurosynth/data/desmtx_cleaned.csv',index_col=0)
-        data=pickle.load(open('../neurosynth/neurosynth_reduced_cleaned.pkl','rb'))
-        print('loading cleaned data')
+    desmtx=pandas.read_csv('data/desmtx_cleaned.csv',index_col=0)
+    data=pickle.load(open('data/neurosynth_reduced_cleaned.pkl','rb'))
+    print('loading cleaned data')
 except:
-    desmtx=pandas.read_csv('../neurosynth/data/desmtx.csv',index_col=0)
-    data=pickle.load(open('../neurosynth/neurosynth_reduced.pkl','rb'))
+    desmtx=pandas.read_csv('data/desmtx.csv',index_col=0)
+    data=pickle.load(open('data/neurosynth_reduced.pkl','rb'))
     # remove pmids without abstracts
     for p in bad_pmids:
         print('removing bad pmid:',p)
@@ -65,17 +59,9 @@ except:
     s=numpy.sum(data,1)
     data=data[s>0,:]
     desmtx=desmtx.ix[s>0]
-    if deduplicate:
-        dupes=desmtx.duplicated()
-        desmtx=desmtx.ix[dupes==False]
-        data=data[dupes.values==False,:]
-        desmtx.to_csv('../neurosynth/data/desmtx_cleaned_deduped.csv')
-        pickle.dump(data,open('../neurosynth/neurosynth_reduced_cleaned_deduped.pkl','wb'))
-        print('created and saved cleaned/deduplicated data')
-    else:
-        desmtx.to_csv('../neurosynth/data/desmtx_cleaned.csv')
-        pickle.dump(data,open('../neurosynth/neurosynth_reduced_cleaned.pkl','wb'))
-        print('created and saved cleaned data')
+    desmtx.to_csv('data/desmtx_cleaned.csv')
+    pickle.dump(data,open('data/neurosynth_reduced_cleaned.pkl','wb'))
+    print('created and saved cleaned data')
 
 assert data.shape[0]==desmtx.shape[0]
 
@@ -93,25 +79,26 @@ def clean_abstract(a):
     docsplit=[wordnet_lemmatizer.lemmatize(i) for i in nltk.tokenize.word_tokenize(abstract) if len(i)>1]
     return docsplit,abstract
 
-if os.path.exists('ns_abstracts_cleaned.pkl'):
-    abstracts_cleaned=pickle.load(open('ns_abstracts_cleaned.pkl','rb'))
+if os.path.exists('data/ns_abstracts_cleaned.pkl'):
+    abstracts_cleaned=pickle.load(open('data/ns_abstracts_cleaned.pkl','rb'))
     print('loaded ')
 else:
     print('cleaning abstracts')
     abstracts_cleaned={}
     for k in abstracts.keys():
         abstracts_cleaned[k],_=clean_abstract(abstracts[k])
-    pickle.dump(abstracts_cleaned,open('ns_abstracts_cleaned.pkl','wb'))
+    pickle.dump(abstracts_cleaned,open('data/ns_abstracts_cleaned.pkl','wb'))
 # get vector projection for each abstract
 ndims=50
 print('loading Doc2Vec model')
-model_docs=Doc2Vec.load('models/doc2vec_trigram_%ddims.model'%ndims)
+model_docs=Doc2Vec.load('../pubmed_word2vec/models/doc2vec_trigram_%ddims.model'%ndims)
 
 print('getting vector projections')
 pmids=list(desmtx.index)
-inferred_vectors=numpy.zeros((len(pmids),ndims))
+inferred_vectors=pandas.DataFrame()
 for i,pmid in enumerate(pmids):
     if numpy.mod(i,1000)==0:
         print(i)
-    inferred_vectors[i,:]=model_docs.infer_vector(abstracts_cleaned[pmid])
-numpy.save('ns_inferred_vectors.npy',inferred_vectors)
+    inferred_vectors[pmid]=model_docs.infer_vector(abstracts_cleaned[pmid])
+inferred_vectors=inferred_vectors.T
+inferred_vectors.to_csv('data/ns_doc2vec_%ddims_projection.csv'%ndims)
