94b9f6b4582078b86b82397008621fb6efd64af9
poldrack <poldrack@gmail.com>
2017-05-14 16:26:21 -0700

    added exclusion for terms and ns pmids

--- pubmed_word2vec/1.get_articles.py
+++ pubmed_word2vec/1.get_articles.py
@@ -20,9 +20,14 @@ with open('journals.txt') as f:
 
 retmax=2000000
 delay=0.5 # delay for pubmed api
+terms_to_exclude=["neural","neuroimaging","brain","positron emission tomography","fMRI", "functional MRI", "functional magnetic resonance imaging"]
+extra_terms=''
+for t in terms_to_exclude:
+    extra_terms=extra_terms+'NOT "%s" '%t
+force_new=True
 
 # get matching pubmed ids
-if os.path.exists('pmids.pkl'):
+if os.path.exists('pmids.pkl') and not force_new:
     print('using cached pmids')
     pmids=pickle.load(open('pmids.pkl','rb'))
 else:
@@ -35,19 +40,28 @@ for j in journals:
         print('adding',j)
     time.sleep(0.5)
     pmids[j]=[]
-    handle = Entrez.esearch(db="pubmed", retmax=retmax,term='"%s"[TA]'%j)
+    searchterm='"%s"[TA]%s'%(j,extra_terms)
+    #print(searchterm)
+    handle = Entrez.esearch(db="pubmed", retmax=retmax,term=searchterm)
     record = Entrez.read(handle)
     handle.close()
     pmids[j]=[int(i) for i in record['IdList']]
     print('found %d records for'%len(pmids[j]),j)
 pickle.dump(pmids,open('pmids.pkl','wb'))
 
+# get neurosynth data so that we can exclude any papers in the database
+nsdata=pandas.read_csv('../neurosynth/data/database.txt',index_col=0,sep='\t')
+ns_pmids=list(set(nsdata.index))
+
 # get abstract text
-if os.path.exists('abstracts.pkl'):
+# also save authors for later filtering
+
+if os.path.exists('abstracts.pkl') and not force_new:
     print('using cached abstracts')
     abstracts=pickle.load(open('abstracts.pkl','rb'))
 else:
     abstracts={}
+    authors=[]
 
 for j in journals:
     if j in abstracts:
@@ -59,6 +73,15 @@ for j in journals:
         abstracts[j]={}
         for i in records['PubmedArticle']:
             pmid=int(i['MedlineCitation']['PMID'])
+            if pmid in ns_pmids:
+                print('skippping neurosynth pmid',pmid)
+                continue
+            if 'AuthorList' in i['MedlineCitation']['Article']:
+                for au in i['MedlineCitation']['Article']['AuthorList']:
+                    if 'LastName' in au:
+                        authors.append(au['LastName'])
+                    else:
+                        print('hmm, no last name',au)
             if 'Abstract' in i['MedlineCitation']['Article']:
                 abstracts[j][pmid]=i['MedlineCitation']['Article']['Abstract']['AbstractText']
             else:
@@ -66,5 +89,7 @@ for j in journals:
 
         print(j,': found %d abstracts from %d keys'%(len(abstracts[j]),len(pmids[j])))
     except:
-        print('problem with',j)
+        e = sys.exc_info()[0]
+        print('problem with',j,e)
 pickle.dump(abstracts,open('abstracts.pkl','wb'))
+pickle.dump(authors,open('authors.pkl','wb'))
