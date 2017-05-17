7936f8f797104e3fb3ea716359bfa4bf2dc894c3
poldrack <poldrack@gmail.com>
2017-05-17 07:15:39 -0700

    change data

--- pubmed_prep/1.3.mk_data_for_mallet.py
+++ pubmed_prep/1.3.mk_data_for_mallet.py
@@ -11,8 +11,8 @@ Created on Sat May 13 16:43:35 2017
 
 import pickle
 
-infile='/Users/poldrack/code/cognitive_encoding_model/pubmed_word2vec/doc_td.pkl'
+infile='/Users/poldrack/code/cognitive_encoding_model/data/pubmed/doc_td.pkl'
 a=pickle.load(open(infile,'rb'))
-with open('/Users/poldrack/code/cognitive_encoding_model/pubmed_word2vec/psych_pubmed_raw.txt','w') as f:
+with open('/Users/poldrack/code/cognitive_encoding_model/data/pubmed/psych_pubmed_raw.txt','w') as f:
     for abs in a:
         f.write('%s EN %s\n'%(abs.tags[0],' '.join(abs.words)))
