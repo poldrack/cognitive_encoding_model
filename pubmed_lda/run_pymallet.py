427ec4b24a609a56b612110a5d249bd7c9674fcd
Russell Poldrack <poldrack@gmail.com>
2017-05-17 09:49:54 -0500

    fix outfile

--- pubmed_lda/run_pymallet.py
+++ pubmed_lda/run_pymallet.py
@@ -18,7 +18,7 @@ ndims=int(sys.argv[1])
 if 1:
     # commands to run smoke test
     pm=PyMallet('/work/01329/poldrack/wrangler/software_wrangler/mallet-2.0.8/',verbose=False)
-    pm.import_data(infile='/work/01329/poldrack/wrangler/code/cognitive_encoding_model/data/pubmed/psych_pubmed_raw.txt')
-    pm.train_topics(ndims,pm.datafile,'../models/lda')
+    print(pm.import_data(infile='/work/01329/poldrack/wrangler/code/cognitive_encoding_model/data/pubmed/psych_pubmed_raw.txt',outfile='../data/pubmed/psych_pubmed.mallet'))
+    print(pm.train_topics(ndims,pm.datafile,'../models/lda'))
     
     
