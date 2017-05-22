5ad56173ae9afa2c9aa848059fc8babd4721d39f
poldrack <poldrack@gmail.com>
2017-05-22 12:16:26 -0700

    update data format

--- encoding_models/3.summarize_prediction_corr_results.py
+++ encoding_models/3.summarize_prediction_corr_results.py
@@ -9,7 +9,7 @@ results={'shuf':{},'noshuf':{}}
 for p in predacc_files_noshuf:
     model=os.path.dirname(p).split('/')[-1]
     p=pickle.load(open(p,'rb'))
-    results['noshuf'][model]={'len':len(p[1]),'mean':numpy.mean(p[1])}
+    results['noshuf'][model]={'len':len(p),'mean':numpy.mean([x[2] for x in p])}
 print(results['noshuf'])
 predacc_files_shuf=[i for i in predacc_files if i.find('shuf')>-1]
 for i,p in enumerate(predacc_files_shuf):
@@ -18,7 +18,7 @@ for i,p in enumerate(predacc_files_shuf):
     p=pickle.load(open(p,'rb'))
     if not model in results['shuf']:
         results['shuf'][model]=[]
-    results['shuf'][model].append(numpy.mean(p[1]))
+    results['shuf'][model].append(numpy.mean([x[2] for x in p]))
 pickle.dump(results,open('predacc_results.pkl','wb'))
 for model in results['shuf'].keys():
     print(model,results['noshuf'][model],scipy.stats.scoreatpercentile(results['shuf'][model],95))
