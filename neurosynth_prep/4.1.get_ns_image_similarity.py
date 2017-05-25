988e6cbf4e2f7177b54ced987ed3dcc88437a232
Russell Poldrack <poldrack@gmail.com>
2017-05-25 08:35:39 -0500

    fix paths

--- neurosynth_prep/4.1.get_ns_image_similarity.py
+++ neurosynth_prep/4.1.get_ns_image_similarity.py
@@ -7,8 +7,9 @@ import numpy,pandas
 from joblib import Parallel, delayed
 from sklearn.metrics import f1_score,jaccard_similarity_score
 
-njobs=46
+njobs=24
 
+data=pickle.load(open('../data/neurosynth/neurosynth_reduced_cleaned.pkl','rb'))
 coords=[]
 for i in range(data.shape[0]):
     for j in range(i,data.shape[0]):
@@ -16,8 +17,7 @@ for i in range(data.shape[0]):
             continue
         coords.append((i,j))
 
-data=pickle.load(open('data/neurosynth_reduced_cleaned.pkl','rb'))
 data=(data>0).astype('int')
 results=Parallel(n_jobs=njobs)(delayed(jaccard_similarity_score)(data[i,:],data[j,:]) for i,j in coords)
 
-pickle.dump(results,open('ns_image_similarity_results.pkl','wb'))
+pickle.dump(results,open('../data/neurosynth/ns_image_similarity_results.pkl','wb'))
