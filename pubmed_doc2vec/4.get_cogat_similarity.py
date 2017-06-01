d378d8fa45b959f68d436c4d848fcf75525714c9
poldrack <poldrack@gmail.com>
2017-06-01 11:45:05 -0700

    fix key error

--- pubmed_doc2vec/4.get_cogat_similarity.py
+++ pubmed_doc2vec/4.get_cogat_similarity.py
@@ -17,7 +17,10 @@ for i in range(desmtx.shape[1]):
         concepts=[]
         for c in [i,j]:
             concepts.append(desmtx.columns[c].replace(' ','_'))
-        concept_similarity[i,j]=m.wv.similarity(concepts[0],concepts[1])
+        try:
+            concept_similarity[i,j]=m.wv.similarity(concepts[0],concepts[1])
+        except KeyError:
+            concept_similarity[i,j]=numpy.nan
 if not os.path.exists('../data/doc2vec'):
     os.mkdir('../data/doc2vec')
 numpy.save(concept_similarity,'../data/doc2vec/cogat_doc2vec_similarity.npy')
